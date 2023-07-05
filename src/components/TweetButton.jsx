bea389a2be6542652b536592af025bd5e6572c1a
Erik Lopez <lopez.erik@gmail.com>
2023-07-05 12:37:15 -0500

    Add ID to component

--- src/components/TweetButton.jsx
+++ src/components/TweetButton.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 
-const TweetButton = ({data}) => {
+const TweetButton = ({data, id}) => {
   console.log('TweetButton', data);
   return (
-    <a className="twitter-share-button" href={`https://twitter.com/intent/tweet?text=${data.text}%20-%20${data.author}&hashtags=FreeCodeCamp,randomQuoteMachine`} data-size="large">Tweet</a>
+    <a className="twitter-share-button" id={id} href={`https://twitter.com/intent/tweet?text=${data?.text}%20-%20${data?.author}&hashtags=FreeCodeCamp,randomQuoteMachine`} data-size="large">Tweet</a>
   )
 }
 
 TweetButton.propTypes = {
   data: PropTypes.object,
+  id: PropTypes.string,
 }
 
 export default TweetButton
