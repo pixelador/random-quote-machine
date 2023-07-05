8e46835919e602dfe054ba3c083a1647b8c87077
Erik Lopez <lopez.erik@gmail.com>
2023-07-05 12:37:32 -0500

    Re-org helper funcs

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,33 @@ import TweetButton from './components/TweetButton';
 const URL = "https://type.fit/api/quotes"
 
 const App = () => {
+  
   const [quoteData, setQuoteData] = useState([]);
   const [dataIndex, setDataIndex] = useState(0);
   const [dataLength, setDataLength] = useState(0);
-
+  
   const getRandomIndex = (length) => Math.floor(Math.random() * length);
   
+  const initializeState = (data) => {
+    setQuoteData(data);
+    setDataLength(data.length);
+    setDataIndex(getRandomIndex(dataLength));
+    console.log('initializeState dataIndex', dataIndex);
+  };
+
+  const handleNewQuoteClick = () => {
+    // Updating dataIndex will trigger a re-render of QuoteBox
+    setDataIndex(getRandomIndex(dataLength));
+    console.log('handleNewQuoteClick dataIndex', dataIndex);
+  };
+
   useEffect(() => {
     // Fetch data from API on initial component mount only
     const fetchData = async () => {
       const output = await fetch(URL);
       const data = await output.json();
       console.log('App data', data);
-      setQuoteData(data);
-      setDataLength(data.length);
-      setDataIndex(getRandomIndex(dataLength));
+      initializeState(data);
     }
 
     fetchData()
@@ -30,22 +42,15 @@ const App = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleNewQuoteClick = () => {
-    // Updating dataIndex will trigger a re-render of QuoteBox
-    setDataIndex(getRandomIndex(dataLength));
-  };
-
   return (
     <>
-    {
     <div id="quote-box">
       <QuoteBox data={quoteData[dataIndex]}/>
       <div className="buttonCol">
         <Button id="new-quote" onClick={handleNewQuoteClick} text="New Quote"/>
-        {/* <TweetButton data={quoteData[dataIndex]}/> */}
+        <TweetButton id="tweet-quote" data={quoteData[dataIndex]}/>
       </div>
     </div>
-    }
     </>
   );
 };
