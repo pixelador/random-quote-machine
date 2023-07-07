e8c0b3900d8904c2419217f894ea1a5b3f3e81af
Erik Lopez <lopez.erik@gmail.com>
2023-07-07 18:09:13 -0500

    Move child components to QuoteBox

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,20 +1,29 @@
 import PropTypes from 'prop-types';
+import TweetButton from './TweetButton';
+import Button from './Button';
 
-const QuoteBox = ({data}) => {
+const QuoteBox = ({data, callback}) => {
   console.log('QuoteBox', data);
   
   return (
     <>
-      <h1 id="text">
-        &quot;{data?.text}&quot;
-      </h1>
-      <h4 id="author">&mdash;&nbsp;{data?.author}</h4>
+      <div id="quote-box">
+        <h1 id="text">
+          &quot;{data?.text}&quot;
+        </h1>
+        <h4 id="author">&mdash;&nbsp;{data?.author}</h4>
+      </div>
+      <div className="buttonRow">
+        <TweetButton data={data}/>
+        <Button id="new-quote" onClick={callback} text="New Quote"/>
+      </div>
     </>
   )
 };
 
 QuoteBox.propTypes = {
   data: PropTypes.object,
+  callback: PropTypes.func,
 }
 
 export default QuoteBox
